d1bc68c6c13a35fb8abcd45d66ccde085d59bc91
Eduardo Aquinta <eddie.ashton@madetech.com>
2023-11-13 11:21:35 +0000

    first commit

---
+++ objectifyURL.js
@@ -0,0 +1,3 @@
+// Converts a URL Query String into an object map
+function convertQueryToMap(query) {
+}
---
+++ objectifyURL.test.js
@@ -0,0 +1,7 @@
+const convertQueryToMap = require("./objectifyURL")
+
+describe("Tests for converting URL to Object", () => {
+    it("", () => {
+
+    })
+})
