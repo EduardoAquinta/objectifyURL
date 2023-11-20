422fcd5dca35950764166b5c4fd8896569f9e4a4
Eduardo Aquinta <eddie.ashton@madetech.com>
2023-11-20 16:03:45 +0000

    3rd test failing

--- objectifyURL.js
+++ objectifyURL.js
@@ -1,3 +1,22 @@
 // Converts a URL Query String into an object map
 function convertQueryToMap(query) {
+    let queryArray = query.split(".");
+    console.log(queryArray)
+    let queryObj = {}
+
+    if(queryArray.length === 1){
+        queryObj = {query};
+        queryObj = Object.values(queryObj)
+    }
+
+       for( let i = 0; i < queryArray.length; i +=2){
+           queryObj[queryArray[i]] = queryArray[(i + 1)]
+       }
+
+    console.log(queryObj)
+    return queryObj;
+
+
 }
+
+module.exports = convertQueryToMap;
--- objectifyURL.test.js
+++ objectifyURL.test.js
@@ -1,7 +1,33 @@
 const convertQueryToMap = require("./objectifyURL")
 
-describe("Tests for converting URL to Object", () => {
-    it("", () => {
+describe("Unit tests for converting URL to Object", () => {
+    it("converts a string to an object", () => {
+        let query = "user"
+        expect(typeof(query)).toBe("string")
+        expect(typeof(convertQueryToMap(query))).toBe("object");
+    })
+    it("converts a . pair within a string into an object key:value pair", () => {
+        let query = "user.name"
+        expect(convertQueryToMap(query)).toStrictEqual({'user' : 'name'})
+    })
+    it("adds user name to the object {'user' : 'name' : {'Bob'} }", () => {
+        let query = "user.name.firstname=Bob"
+        expect(convertQueryToMap(query)).toStrictEqual({'user' : { 'name' : { 'firstname' : 'Bob'}}})
+    })
+})
 
+describe.skip("Assertion Test", () => {
+    it("the assertion test that comes with the kata", () => {
+        let q = 'user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue'
+        let out = {
+                'user': {
+                    'name': {
+                        'firstname': 'Bob',
+                        'lastname': 'Smith'
+                    },
+                    'favoritecolor': 'Light Blue'
+                }
+            };
+        expect(convertQueryToMap(q)).toBe(out);
     })
 })
